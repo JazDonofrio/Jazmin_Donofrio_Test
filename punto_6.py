d77e004b30d4c240d44589e7c7838ecfdd90d37f
JazDonofrio <jazmindonofrio@gmail.com>
2021-10-12 16:04:49 -0300

    Punto 6 - Prueba

---
+++ punto_6.py
@@ -0,0 +1,10 @@
+
+def restaDeNumeros(numero1, numero2):
+    #Toma como parametro dos numeros y los resta
+    return numero1 - numero2
+
+#Pasamos el resultado a una variable llamada resultado para despues imprimirla.
+resultado = restaDeNumeros(10, 5)
+
+#Se imprime el resultado de la resta
+print("El resultado de la resta es: ", resultado)
