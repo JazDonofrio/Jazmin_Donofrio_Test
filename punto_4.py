ee01e3e8c8e4244d1df89904975bc72893c8891c
JazDonofrio <jazmindonofrio@gmail.com>
2021-10-12 16:03:12 -0300

    Punto 4 - Prueba

---
+++ punto_4.py
@@ -0,0 +1,20 @@
+"""    c- Genere un ejemplo del bucle "for" en el cual recorra los elementos de una lista 
+        y los imprima por consola.
+    d- Genere un ejemplo del bucle "while" en el cual se imprima 5 veces la siguiente 
+        frase por consola: "La condición se cumplio."  """
+        
+#Lista de numeros
+numeros = [1, 2, 3, 4, 5, 6]
+
+for elemento in numeros:
+    print(elemento)
+
+# condicion = 0
+
+# while condicion < 5:
+    
+#     print("La condición se cumplio :) ") 
+#     condicion += 1 
+
+      
+    
